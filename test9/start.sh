0486c4779f28e8e5382257a7e07fa08f43e052bc
System Administrator <root@yishiyaonie.local>
2015-12-10 17:43:57 +0800

    38

--- test9/start.sh
+++ test9/start.sh
@@ -4,6 +4,7 @@ modprobe dm-mod
 insmod dm-cache.ko
 echo 0 62914560 cache /dev/vdc /dev/vdb | dmsetup create pcache
 dmsetup status
+mkfs.ext4 /dev/mapper/pcache
 mount /dev/mapper/pcache /mnt/dmcache/
 cd /mnt/dmcache/
 
