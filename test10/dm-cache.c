b417aa699461fcedf115cf75526bbe6d1356f2da
System Administrator <root@yishiyaonie.local>
2015-12-11 16:04:37 +0800

    50

--- test10/dm-cache.c
+++ test10/dm-cache.c
@@ -1714,18 +1714,18 @@ static int precache_read_miss(struct cache_c *dmc, struct bio* bio, sector_t cac
     bio->bi_bdev = dmc->src_dev->bdev;  
     dmc->step4++;
 
-	for (i=3; i<4 ; i++)
-	{
+	//for (i=3; i<4 ; i++)
+	//{
 		
-		request_block=request_block+(i << dmc->block_shift);
-		cache[cache_block].src_cache= request_block;
-		cache[cache_block].dmc= dmc;
-		cache[cache_block].dest_cache= cache_block;
-		cache_insert(dmc, request_block, cache_block);
-	    pre_back(dmc, cache_block,request_block, 1);
-	    precache_lookup(dmc, 62914561, &cache_block);
+		//request_block=request_block+(i << dmc->block_shift);
+	//	cache[cache_block].src_cache= request_block;
+	//	cache[cache_block].dmc= dmc;
+	//	cache[cache_block].dest_cache= cache_block;
+	//	cache_insert(dmc, request_block, cache_block);
+	//    pre_back(dmc, cache_block,request_block, 1);
+	 //   precache_lookup(dmc, 62914561, &cache_block);
 
-	} 
+	//} 
 
 	
 	return 1;
