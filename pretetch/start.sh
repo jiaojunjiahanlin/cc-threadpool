1a70520f69f3a12e0823a989d78848b9f7b1335d
System Administrator <root@yishiyaonie.local>
2015-12-12 22:14:09 +0800

    60

---
+++ pretetch/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+modprobe dm-mod
+insmod dm-cache.ko
+echo 0 62914560 cache /dev/vdc /dev/vdb | dmsetup create pcache
+dmsetup status
+mkfs.ext4 /dev/mapper/pcache
+mount /dev/mapper/pcache /mnt/dmcache/
+cd /mnt/dmcache/
+
+
+dd if=/dev/zero of=2G.file bs=2G count=1
+
+dmsetup status
