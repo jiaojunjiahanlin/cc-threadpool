c98d6c3aec8940df721ab33d2fd98e98318854a8
System Administrator <root@yishiyaonie.local>
2015-12-14 15:39:29 +0800

    75

---
+++ pb/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+modprobe dm-mod
+insmod dm-cache.ko
+echo 0 62914560 cache /dev/vdc /dev/vdb 0 32 65536 256 1| dmsetup create pcache
+dmsetup status
+mkfs.ext4 /dev/mapper/pcache
+mount /dev/mapper/pcache /mnt/dmcache/
+cd /mnt/dmcache/
+
+
+dd if=/dev/zero of=2G.file bs=2G count=1
+
+dmsetup status
