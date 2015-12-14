5c474b23e2f59c71a934e18d041d4bf634c4d843
System Administrator <root@yishiyaonie.local>
2015-12-14 21:51:31 +0800

    83

--- pretetch/test.sh
+++ pretetch/test.sh
@@ -1,1 +1,1 @@
-fio -filename=/mnt/dmcache/2G.file -direct=1 -iodepth 1 -thread -rw=read -ioengine=psync -bs=512k -size=2G -numjobs=10 -runtime=1000 -group_reporting -name=mytest
+fio -filename=/mnt/dmcache/2G.file -direct=1 -iodepth 1 -thread -rw=randread -ioengine=psync -bs=512k -size=200M -numjobs=2 -runtime=1000 -group_reporting -name=mytest
